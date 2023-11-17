9d4ad0278aca1ab553d4d0a41c35b17004fc72a5
sidhupp6@gmail.com <sidhupp6@gmail.com>
2023-11-17 10:32:01 +0000

    Created Form2; Edited height, Form1 and login

--- client_code/height/__init__.py
+++ client_code/height/__init__.py
@@ -11,3 +11,7 @@ class height(heightTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('Form2')
