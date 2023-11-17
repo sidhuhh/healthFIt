698e552759cd2d85bbce04aeb51cb045963acaf2
sidhupp6@gmail.com <sidhupp6@gmail.com>
2023-11-17 13:17:35 +0000

    Edited BMI_cal and sex; Deleted height and weight

--- client_code/sex/__init__.py
+++ client_code/sex/__init__.py
@@ -26,5 +26,5 @@ class sex(sexTemplate):
 
   def button_4_click(self, **event_args):
     """This method is called when the button is clicked"""
-    open_form('weight')
+    open_form('BMI_cal')
 
